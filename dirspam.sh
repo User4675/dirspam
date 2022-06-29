a3edbd66189294631c13fb95262d184ce80a5225
User4675 <business@jakemayor.com>
2022-06-29 18:59:10 +0100

    Create dirspam.sh

---
+++ dirspam.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for i in {1..1500}
+do
+
+    mkdir -p ./$i/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/dirspam/
+
+done
